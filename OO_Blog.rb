0515aa6e2b4da1c9dbd372b98a03e6199a7fa4fa
Tom Walchli <tom@wit-americas.com>
2015-01-13 17:07:05 -0500

    Done!

--- OO_Blog.rb
+++ OO_Blog.rb
@@ -1,17 +1,36 @@
 class Blog
+	def initialize
+		@posts = []
+		@posts.push(Post.new)
+		update
+	end
 
+	def update
+		puts "\n\n"
+		@posts.each do |post|
+			puts "#{post.time}: #{post.title}"
+			puts "************************"
+			puts post.text
+			puts "------------------------"
+		end
+		puts "\n"
+		@posts.push(Post.new)
+		update
+	end
 end
 
 class Post
-	attr_reader :date, :title, :text
+	attr_reader :time, :title, :text
 	def initialize
-		input
-
+		@title = input("Type title:")
+		@text  = input("Type text:")
+		@time  = Time.new
 	end
 
-	def input
-		@
+	def input(phrase)
+		puts phrase
+		return gets.chomp
 	end
-
 end
 
+Blog.new
